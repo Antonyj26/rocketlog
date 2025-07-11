0080d3f1a6f2dfbbd8100de0ca0ab135de344077
Antonyj26 <antonyjosu26@gmail.com>
2025-07-11 01:16:11 -0300

    Creating Authenticated and Authorization

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { usersRouter } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
+import { deliveriesRoutes } from "./deliveries-routes";
 
 const route = Router();
 
 route.use("/users", usersRouter);
 route.use("/sessions", sessionsRoutes);
+route.use("/deliveries", deliveriesRoutes);
 
 export { route };
