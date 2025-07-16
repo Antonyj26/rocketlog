45d9aa40a0bda38cafddc935a43353b65f26c088
Antonyj26 <antonyjosu26@gmail.com>
2025-07-16 17:42:20 -0300

    Creating Controller and Delivery Log Route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import { usersRouter } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
 import { deliveriesRoutes } from "./deliveries-routes";
+import { deliveryLogsRoutes } from "./delivery-logs-routes";
 
 const route = Router();
 
 route.use("/users", usersRouter);
 route.use("/sessions", sessionsRoutes);
 route.use("/deliveries", deliveriesRoutes);
+route.use("/delivery-logs", deliveryLogsRoutes);
 
 export { route };
