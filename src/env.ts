247d4de19c87a29818cd61b7c0957a52ea725d11
Antonyj26 <antonyjosu26@gmail.com>
2025-07-19 16:46:56 -0300

    .

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchmea = z.object({
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string(),
+  PORT: z.coerce.number().default(3333),
 });
 
 export const env = envSchmea.parse(process.env);
